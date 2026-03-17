891901f229744a49b5e52535f8491a9966705273
Arun <arunkumard0606@gmail.com>
2026-03-17 22:51:26 +0530

    added initializers

--- ch_1/Sample.java
+++ ch_1/Sample.java
@@ -1,4 +1,6 @@
 package ch_1;
+import java.util.*;
+import java.util.List;
 //Public Class name should be same as file name --> Sample.java else Doesnt' compile
 public class Sample {
    public static void main( String []  args) {
@@ -32,3 +34,32 @@ Main Method rules :
 4) public final static void main(final String[] args) {} - Possible
  */
 
+
+
+//Imports use wildcard "*" -> only classes
+
+class Imports{
+    /*
+    import java.util.*.*; -> invalid
+    Explicit import > wildcard import
+     */
+
+    public static void main(String[] args) {
+        List<String> ls= List.of("Arun");
+        System.out.println(ls.get(0));
+    }
+}
+
+class InstanceInitializer{
+    private int age=0;
+    {age=15;}
+    public static void main(String[] args) {
+
+        InstanceInitializer instanceInitializer1 = new InstanceInitializer();
+        instanceInitializer1.age=10;
+        System.out.println(instanceInitializer1.age);  //10
+
+        InstanceInitializer instanceInitializer2 = new InstanceInitializer();
+        System.out.println(instanceInitializer2.age);  //15
+    }
+}
